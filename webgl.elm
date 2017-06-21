b23e197c71e97c4d288a66c73c89cbf0dcf3b073
Jens Egholm <jensegholm@protonmail.com>
2017-06-21 17:22:45 +0200

    Stress test

--- webgl.elm
+++ webgl.elm
@@ -8,6 +8,7 @@ import AnimationFrame
 import Html exposing (Html, text, div)
 import Html.Attributes exposing (width, height, style)
 import Math.Matrix4 as Mat4 exposing (Mat4)
+import Math.Vector2 as Vec2 exposing (Vec2, vec2)
 import Math.Vector3 as Vec3 exposing (Vec3, vec3)
 import Time exposing (Time)
 import WebGL exposing (Mesh, Shader, Entity)
@@ -15,10 +16,12 @@ import Platform.Sub
 import Keyboard
 import Keyboard.Key exposing (..)
 import Window
+import Mouse
 import Task
 
 type alias Model =
   { window : Window.Size
+  , mouse : Maybe Vec2
   , time : Time
   , person : Person
   }
@@ -26,7 +29,8 @@ type alias Model =
 type Msg =
     Resize Window.Size
   | Tick Float
-  | Move Vec3
+  | MoveMouse Vec2
+  | MoveKey Vec2
 
 type alias Person =
     { position : Vec3 }
@@ -34,7 +38,7 @@ type alias Person =
 main : Program Never Model Msg
 main =
     Html.program
-        { init = ( { window = (Window.Size 0 0), time = 0, person = Person (vec3 0 0 -5)}, Task.perform Resize Window.size )
+        { init = ( { window = (Window.Size 0 0), mouse = Nothing, time = 0, person = Person (vec3 0 0 -5)}, Task.perform Resize Window.size )
         , view = view
         , subscriptions = subscriptions
         , update = update
@@ -44,21 +48,35 @@ subscriptions : Model -> Sub Msg
 subscriptions model = Sub.batch
   [ AnimationFrame.diffs Tick
   , Keyboard.presses keyPress
+  , Mouse.moves mouseMove
   ]
 
 keyPress : Keyboard.KeyCode -> Msg
 keyPress code =
   case (Keyboard.Key.fromCode code) of
-    Up -> Move (vec3 0 0 1)
-    Down -> Move (vec3 0 0 -1)
-    e -> Move (vec3 0 0 0)
+    Up -> MoveKey (vec2 0 1)
+    Down -> MoveKey (vec2 0 -1)
+    Left -> MoveKey (vec2 1 0)
+    Right -> MoveKey (vec2 -1 0)
+    e -> MoveKey (vec2 0 0)
 
+mouseMove : Mouse.Position -> Msg
+mouseMove {x, y} = MoveMouse (vec2 (toFloat x) (toFloat y))
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Tick elapsed -> ( { model | time = model.time + elapsed}, Cmd.none )
-    Move v -> { model | person = Person (Vec3.add model.person.position v) } ! []
+    MoveKey v -> { model | person = Person (Vec3.add model.person.position (vec3 (Vec2.getX v) 0 (Vec2.getY v))) } ! []
+    MoveMouse v -> case model.mouse of
+      Nothing -> { model | mouse = Just v } ! []
+      Just mouse ->
+        let
+          scale = (Vec3.getZ model.person.position) / 1000
+          mouseDelta = Vec2.scale scale (Vec2.sub mouse v)
+          mouse3D = vec3 (Vec2.getX mouseDelta) (Vec2.getY mouseDelta) 0
+        in
+        { model | mouse = Just v, person = Person (Vec3.add model.person.position mouse3D) } ! []
     Resize newSize -> { model | window = newSize } ! []
 view : Model -> Html msg
 view model =
@@ -88,7 +106,12 @@ type alias Vertex =
 
 lineModel : Mesh Vertex
 lineModel =
-    WebGL.lineStrip [ Vertex (vec3 0 0 0) (vec3 0 0 0), Vertex (vec3 1 1 0) (vec3 -1 -1 0) ]
+    let
+      l x y = ( Vertex (vec3 (toFloat x / 10) 0 (toFloat y / 10)) (vec3 0 0 0), Vertex (vec3 1 (toFloat x / 10) 0) (vec3 -1 -1 0) )
+      xs = List.range 0 1000
+      ys = List.range 0 1000
+    in
+      WebGL.lines <| List.map2 l xs ys
 
 -- Shaders
 type alias Uniforms =
